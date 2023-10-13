797a206a54fc0745675830620dc318647af117bf
Aileru Abdulqoyyum <aileruenterprise@gmail.com>
2023-10-13 07:17:23 -0400

    Filtered master branch

--- tekton_pipelinerun.sh
+++ tekton_pipelinerun.sh
@@ -10,11 +10,11 @@ oc login  -u system:admin --token=$token --server=$server
 # Clone the Django backend, build image and deploy.
 GH_REPO_URL='https://github.com/Olagold-hackxx/Climate_wavers_DjangoBackend_microservice.git'
 PIPELINE='build-and-deploy'
-WORKSPACE=shared-work
+WORKSPACE=shared-workspace
 DEPLOY_NAME=ClimateWavers-Django
 IMAGE='quay.io/olagolhackxx/climatewavers_django:v1'
 export volumeClaimTemplateFile
-volumeClaimTemplateFile="$(dirname -- "$0")/persistent_volume_claim.yaml"
+volumeClaimTemplateFile="$(dirname -- "$0")/k8s/persistent_volume_claim.yaml"
 echo Start pipeline $PIPELINE:
 echo "Building source code from" $GH_REPO_URL
 echo "To image at location" $IMAGE
