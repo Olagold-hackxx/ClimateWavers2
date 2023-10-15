0f3e894ec03d0ca1765354aade33f1cee0fc8555
Aileru Abdulqoyyum <aileruenterprise@gmail.com>
2023-10-15 12:55:52 -0400

    Groupp dtdataset

---
+++ PredictiveAnalysisModel/get_climate_data.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+reader = pd.read_csv('dataset/historical-disasters.csv')
+disaster_types = ["Storm", "Flood", "Epidemic", "Earthquake", "Drought", "Volcanic activity", "Wildfire"]
+for disaster in disaster_types:
+	writer = reader[reader['Disaster Type']==disaster]
+	writer.to_csv('{}.csv'.format(disaster), index=False)
