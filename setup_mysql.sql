f8fda7517ce09531348736a77902a44dbdc471de
IsmaelKiprop <kipropismael27@gmail.com>
2023-10-16 21:24:18 +0300

    mariadb confg

--- setup_mysql.sql
+++ setup_mysql.sql
@@ -1,7 +1,7 @@
 -- prepares a MySQL server for the ClimateWavers
 
-CREATE DATABASE IF NOT EXISTS climatwavers_db;
-CREATE USER IF NOT EXISTS 'climatwavers_dev'@'localhost' IDENTIFIED BY 'climatwavers_dev_pwd';
-GRANT ALL PRIVILEGES ON `climatwavers_db`.* TO 'climatwavers_dev'@'localhost';
-GRANT SELECT ON `performance_schema`.* TO 'climatwavers_dev'@'localhost';
+CREATE DATABASE IF NOT EXISTS 'climatewavers_db';
+CREATE USER IF NOT EXISTS 'climatewavers'@'localhost' IDENTIFIED BY 'waverx';
+GRANT ALL PRIVILEGES ON 'climatewavers_db'.* TO 'climatewavers'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'climatewavers'@'localhost';
 FLUSH PRIVILEGES
