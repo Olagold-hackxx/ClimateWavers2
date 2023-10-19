d3161edd6ec34383a24fbb93eca82d60a9bfe203
Ismael Tech <133222922+IsmaelKiprop@users.noreply.github.com>
2023-10-19 07:12:54 +0300

    Documented urls.py

--- climate_wavers/urls.py
+++ climate_wavers/urls.py
@@ -4,25 +4,24 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("n/login", views.login_view, name="login"),
-    path("n/logout", views.logout_view, name="logout"),
-    path("n/register", views.register, name="register"),
-    path("<str:username>", views.profile, name="profile"),
-    path("n/following", views.following, name="following"),
-    path("n/saved", views.saved, name="saved"),
-    path("n/createpost", views.create_post, name="createpost"),
-    path("n/community/<int:id>/like", views.like_post, name="likepost"),
-    path("n/community/<int:id>/unlike", views.unlike_post, name="unlikepost"),
-    path("n/community/<int:id>/save", views.save_post, name="savepost"),
-    path("n/community/<int:id>/unsave", views.unsave_post, name="unsavepost"),
-    path("n/community/<int:post_id>/comments", views.comment, name="comments"),
-    path("n/community/<int:post_id>/write_comment", views.comment, name="writecomment"),
-    path("n/community/<int:post_id>/delete", views.delete_post, name="deletepost"),
-    path("<str:username>/follow", views.follow, name="followuser"),
-    path("<str:username>/unfollow", views.unfollow, name="unfollowuser"),
-    path("n/community/<int:post_id>/edit", views.edit_post, name="editpost"),
+    path("", views.index, name="index"),  # Homepage
+    path("n/login", views.login_view, name="login"),  # Login page
+    path("n/logout", views.logout_view, name="logout"),  # Logout page
+    path("n/register", views.register, name="register"),  # Registration page
+    path("<str:username>", views.profile, name="profile"),  # User profile page
+    path("n/following", views.following, name="following"),  # Following page
+    path("n/saved", views.saved, name="saved"),  # Saved posts page
+    path("n/createpost", views.create_post, name="createpost"),  # Create a new post page
+    path("n/community/<int:id>/like", views.like_post, name="likepost"),  # Like a post
+    path("n/community/<int:id>/unlike", views.unlike_post, name="unlikepost"),  # Unlike a post
+    path("n/community/<int:id>/save", views.save_post, name="savepost"),  # Save a post
+    path("n/community/<int:id>/unsave", views.unsave_post, name="unsavepost"),  # Unsave a post
+    path("n/community/<int:post_id>/comments", views.comment, name="comments"),  # View comments on a post
+    path("n/community/<int:post_id>/write_comment", views.comment, name="writecomment"),  # Write a comment on a post
+    path("n/community/<int:post_id>/delete", views.delete_post, name="deletepost"),  # Delete a post
+    path("<str:username>/follow", views.follow, name="followuser"),  # Follow a user
+    path("<str:username>/unfollow", views.unfollow, name="unfollowuser"),  # Unfollow a user
+    path("n/community/<int:post_id>/edit", views.edit_post, name="editpost"),  # Edit a post
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # Serve media files during development
