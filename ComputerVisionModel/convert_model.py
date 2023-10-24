5bf1167721829da7af056d46543b1485d39a22ff
Aileru Abdulqoyyum <aileruenterprise@gmail.com>
2023-10-24 15:52:47 +0100

    AI model

--- ComputerVisionModel/convert_model.py
+++ ComputerVisionModel/convert_model.py
@@ -1,9 +1,13 @@
-from pathlib import Path
 from openvino import convert_model
 import tensorflow as tf
+import openvino as ov
 
 model = tf.keras.models.load_model('model/disaster_detector.h5')
 # The paths of the source and converted models
 
-ov_model = convert_model(input_model=model,
-                         input=[1,224,224,3])
+ov_model = convert_model(model)
+
+
+# save model to OpenVINO IR for later use
+ov.save_model(ov_model, 'model/model.xml')
+
