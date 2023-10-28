a21c3eff3f188dced4cfae5d8dbdf90aea471594
IsmaelKiprop <kipropismael27@gmail.com>
2023-10-28 20:57:40 +0300

    RealTimeNotification

--- climate_wavers/routing.py
+++ climate_wavers/routing.py
@@ -1,13 +1,12 @@
-from channels.routing import ProtocolTypeRouter, URLRouter
 from django.urls import re_path
+from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from yourapp import consumers
+from climate_wavers.consumers import YourConsumer  # Import your consumer class
 
 application = ProtocolTypeRouter({
     "websocket": AuthMiddlewareStack(
         URLRouter([
-            re_path(r"ws/some_path/", consumers.YourConsumer.as_asgi()),
+            re_path(r"ws/notifications/", YourConsumer.as_asgi()),  # Define your WebSocket path
         ]),
     ),
 })
-
