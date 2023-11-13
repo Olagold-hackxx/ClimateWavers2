d8688390f0b4c9f112591f6d1ff6eecf3244befa
IsmaelKiprop <kipropismael27@gmail.com>
2023-11-13 19:14:39 +0300

    updated

--- automate_deployment/pipeline.sh
+++ automate_deployment/pipeline.sh
@@ -10,11 +10,11 @@ server=$SERVER
 #Login to openshift cli
 oc login  --token=$token --server=$server
 # Clone the Django backend, build image and deploy.
-GH_REPO_URL='https://github.com/Olagold-hackxx/Climate_wavers_DjangoBackend_microservice.git'
+GH_REPO_URL='https://github.com/ClimateWavers/waverX-NLP.git'
 PIPELINE='build-and-deploy'
 WORKSPACE=shared-work
-DEPLOY_NAME=climatewavers-backend
-IMAGE='quay.io/olagolhackxx/climatewavers-djangobackend:v1'
+DEPLOY_NAME=waverx-nlp
+IMAGE='quay.io/olagolhackxx/waverx-nlp:v1'
 export volumeClaimTemplateFile
 volumeClaimTemplateFile="$(dirname -- "$0")/k8s/persistent_volume_claim.yaml"
 echo Start pipeline $PIPELINE:
